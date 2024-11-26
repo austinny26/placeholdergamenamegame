46e44b76b4365f49e85c2f2a4138753d4b7c695f
austi <abc@gmail.com>
2024-11-25 21:31:50 -0500

    added a message when program starts

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,9 +1,20 @@
 package main;
 
 public class MainClass {
-
     public static void main(String[] args) {
+        int rand;
+        rand = (int)(Math.random()*2);
+
+        if(rand == 1){
+            System.out.println("hello WORLD");
+        }
+        else {
+            System.out.println("HELLOOOOOO WORLD");
+        }
+
+
         new Game();
+
     }
 
 }
